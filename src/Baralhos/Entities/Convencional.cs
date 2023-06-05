1226505c602fbc3ae1f69d7e3490d5938edb6ef2
PEDRO HENRIQUE EVERS <pedroevers@gmail.com>
2023-06-05 15:57:36 -0300

    Primeiro commit

---
+++ src/Baralhos/Entities/Convencional.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baralhos.Entities
+{
+    public class Convencional
+    {
+        public int Id { get; set; }
+        public int TotalMonte { get; set; }
+        public int TotalLixeira { get; set; }
+        public int TotalEmJogo { get; set; }
+        public List<int> Monte { get; set; }
+        public List<int> Lixeira{ get; set; }
+        public List<int> EmJogo { get; set; }
+
+        public void Inicio()
+        {
+            string coringa = "0";
+            do
+            {
+                Console.WriteLine("Com coringa?");
+                Console.WriteLine("1 = S | 2 = N");
+                coringa = Console.ReadLine();
+            } while (coringa != "1" || coringa != "2");
+            for(int i = 0; i<52; i++)
+            {
+                Monte[i]= i+1;
+            }
+            Lixeira.Clear();
+            EmJogo.Clear();
+            TotalMonte = 52;
+            TotalLixeira = 0;
+            TotalEmJogo = 0;
+
+            Console.WriteLine("Baralho criado");
+        }
+    }
+}
