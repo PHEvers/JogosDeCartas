8ec751068cf0fd43a4ff85094460a3961e09517e
PEDRO HENRIQUE EVERS <pedroevers@gmail.com>
2023-06-06 17:27:48 -0300

    Leitor finalizado

--- src/Baralhos/Entities/Convencional.cs
+++ src/Baralhos/Entities/Convencional.cs
@@ -68,38 +68,120 @@ namespace Baralhos.Entities
         }
         public string Leitor(int i)
         {
-            if( (i/13) < 1)
+            if( (i/14) < 1)
             {
-                if(i < 11)
+                if (i == 1)
+                {
+                    Console.WriteLine("Ás Espadas");
+                    return "Ás Espadas";
+                }
+                if (i < 11)
                 {
                     Console.WriteLine(i + " Espadas");
                     return i + " Espadas";
                 }
+                if (i == 11)
+                {
+                    Console.WriteLine("Valete Espadas");
+                    return "Valete Espadas";
+                }
+                if (i == 12)
+                {
+                    Console.WriteLine("Dama Espadas");
+                    return "Dama Espadas";
+                }
+                if (i == 13)
+                {
+                    Console.WriteLine("Rei Espadas");
+                    return "Rei Espadas";
+                }
             }
-            if ( (i/13) < 2)
+            if ( (i/14) < 2)
             {
+                if (i == 14)
+                {
+                    Console.WriteLine("Ás Ouros");
+                    return "Ás Ouros";
+                }
                 if ( i%13 < 11 && i % 13 > 0)
                 {
                     Console.WriteLine(i % 13 + " Ouros");
                     return i + " Ouros";
                 }
+                if (i == 24)
+                {
+                    Console.WriteLine("Valete Ouros");
+                    return "Valete Ouros";
+                }
+                if (i == 25)
+                {
+                    Console.WriteLine("Dama Ouros");
+                    return "Dama Ouros";
+                }
+                if (i == 26)
+                {
+                    Console.WriteLine("Rei Ouros");
+                    return "Rei Ouros";
+                }
+
             }
-            if ((i / 13) < 3)
+            if ((i / 14) < 3)
             {
+                if (i == 27)
+                {
+                    Console.WriteLine("Ás Copas");
+                    return "Ás Copas";
+                }
                 if (i % 13 < 11 && i % 13 > 0)
                 {
                     Console.WriteLine(i % 13 + " Copas");
                     return i + " Copas";
                 }
+                if (i == 37)
+                {
+                    Console.WriteLine("Valete Copas");
+                    return "Valete Copas";
+                }
+                if (i == 38)
+                {
+                    Console.WriteLine("Dama Copas");
+                    return "Dama Copas";
+                }
+                if (i == 39)
+                {
+                    Console.WriteLine("Rei Copas");
+                    return "Rei Copas";
+                }
             }
-            if ((i / 13) < 4)
+            if ((i / 14) < 4)
             {
+                if (i == 40)
+                {
+                    Console.WriteLine("Ás Paus");
+                    return "Ás Paus";
+                }
                 if (i % 13 < 11 && i % 13 > 0)
                 {
                     Console.WriteLine(i % 13 + " Paus");
                     return i + " Paus";
                 }
+                if (i == 50)
+                {
+                    Console.WriteLine("Valete Paus");
+                    return "Valete Paus";
+                }
+                if (i == 51)
+                {
+                    Console.WriteLine("Dama Paus");
+                    return "Dama Paus";
+                }
+                if (i == 52)
+                {
+                    Console.WriteLine("Rei Paus");
+                    return "Rei Paus";
+                }
             }
+
             Console.WriteLine(i + " Vazamento");
             return "seila";
         } 
